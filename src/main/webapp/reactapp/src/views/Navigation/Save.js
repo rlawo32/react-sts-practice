ef5b551797b18e6ec2ffd8803bab15c1f7490229
rlawo <rlawo32@naver.com>
2023-08-08 23:09:35 +0900

    [react-project] 게시판_작업_4_230808

--- src/main/webapp/reactapp/src/views/Navigation/Save.js
+++ src/main/webapp/reactapp/src/views/Navigation/Save.js
@@ -1,11 +1,36 @@
-import React, { useState } from "react";
+import React, {useEffect, useState, useRef} from "react";
+import '@toast-ui/editor/dist/toastui-editor.css';
+import '@toast-ui/editor/dist/theme/toastui-editor-dark.css';
+import 'tui-color-picker/dist/tui-color-picker.css';
+import '@toast-ui/editor-plugin-color-syntax/dist/toastui-editor-plugin-color-syntax.css';
+import colorSyntax from '@toast-ui/editor-plugin-color-syntax';
+import { Editor } from '@toast-ui/react-editor';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
+import Box from '@mui/material/Box';
 import axios from "axios";
 
 const Save = () => {
+    const editorRef = useRef(null);
+
+    const editorSaveHandler = () => {
+        let markDownContent = editorRef.current.getInstance().getMarkdown();
+        let htmlContent = editorRef.current.getInstance().getHTML();
+
+        console.log(markDownContent);
+        console.log("================================");
+        console.log(htmlContent);
+    }
+
+    const colorSyntaxOptions = {
+        preset: [
+            "#333333", "#666666", "#FFFFFF", "#EE2323", "#F89009", "#009A87", "#006DD7", "#8A3DB6",
+            "#781B33", "#5733B1", "#953B34", "#FFC1C8", "#FFC9AF", "#9FEEC3", "#99CEFA", "#C1BEF9",
+        ],
+    };
+
     const accessToken = new URL(window.location.href).searchParams.get("accessToken");
     const refreshToken = new URL(window.location.href).searchParams.get("refreshToken");
 
@@ -80,8 +105,27 @@ const Save = () => {
 
                     <Form.Group className="mb-3" controlId="boardContent">
                         <Col sm>
-                            <Form.Control as="textarea" rows={3} placeholder="내용을 입력하세요" value={boardContent} onChange={boardContentChangeHandler}/>
-                            <Form.Text className="text-muted"></Form.Text>
+                            {/*<Form.Control as="textarea" rows={3} placeholder="내용을 입력하세요" value={boardContent} onChange={boardContentChangeHandler}/>*/}
+                            {/*<Form.Text className="text-muted"></Form.Text>*/}
+                            <Box>
+                                <Editor
+                                    ref={editorRef}
+                                    height="300px"
+                                    initialValue="내용을 입력하세요."
+                                    previewStyle="vertical" // or "tab"
+                                    initialEditType="wysiwyg" // or "markdown"
+                                    // hideModeSwitch={true} // wysiwyg markdown 전환 버튼 숨기기
+                                    toolbarItems={[ // 툴바 옵션 설정
+                                        ["heading", "bold", "italic", "strike"],
+                                        ["hr", "quote"],
+                                        ["table", "image", "link"],
+                                        ["code", "codeblock"]
+                                    ]}
+                                    theme="dark" // dark 모드
+                                    usageStatistics={false} // 구글 통계 수집 거부
+                                    plugins={[[colorSyntax, colorSyntaxOptions]]} // 플러그인
+                                />
+                            </Box>
                         </Col>
                     </Form.Group>
 
@@ -90,6 +134,9 @@ const Save = () => {
                     </div>
                     <div className="d-grid gap-1" style={ {margin:"5px"} }>
                         <Button variant="secondary" href="/">취소</Button>
+                    </div>
+                    <div className="d-grid gap-1" style={ {margin:"5px"} }>
+                        <Button variant="secondary" onClick={editorSaveHandler}>테스트</Button>
                     </div>
                 </Form>
             </Container>
