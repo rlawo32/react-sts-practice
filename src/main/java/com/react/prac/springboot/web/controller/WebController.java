8101bf9b1cecc54b3b00cb3d611de34642b6084e
rlawo <rlawo32@naver.com>
2023-10-12 09:19:21 +0900

    [react-project] 새로고침_작업_231012 (1)

---
+++ src/main/java/com/react/prac/springboot/web/controller/WebController.java
@@ -0,0 +1,30 @@
+package com.react.prac.springboot.web.controller;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequiredArgsConstructor
+@Controller
+public class WebController implements ErrorController {
+
+    @RequestMapping(value = "/error")
+    public String handleNoHandlerFoundException() {
+//        int status = response.getStatus();
+//
+//        System.out.println(status);  //오류 상태
+//        System.out.println(request.getRequestURI());  //요청 주소
+
+//        if (Objects.equals(request.getContentType(), MediaType.APPLICATION_JSON_VALUE)) {
+//            Map<String, Object> body = Map.of("error", "Not Found", "timestamp", System.currentTimeMillis());
+//
+//            System.out.println(body.get(0));
+//
+//            return new ResponseEntity<>(body, HttpStatus.NOT_FOUND);
+//        }
+
+//        return new ResponseEntity<>("Not Found", HttpStatus.NOT_FOUND);
+        return "/index.html";
+    }
+
+}
